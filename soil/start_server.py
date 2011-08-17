6ba894f937a7c418f0e9072aa50833f7b55684be
seungjin <seungjin@seungjin.net>
2011-08-17 14:18:00 -0700

    ssl with tornado

--- soil/start_server.py
+++ soil/start_server.py
@@ -11,15 +11,29 @@ from django.conf import settings
 
 #soil libraries
 from cloudservice.hello import *
+from cloudservice.http_client import Http_client
 
+
+from ConfigParser import RawConfigParser
+config = RawConfigParser()
+config.read('/Users/seungjin/Documents/soil_home/creadentials.ini')
 
 application = tornado.web.Application([
   (r"/cloudservice/hello", HelloHandler),
+  (r"/cloudservice/http_client_test", Http_client),
 ])
 
 
 # start the server
 if __name__ == "__main__":
-  http_server = tornado.httpserver.HTTPServer(application)
+  http_server = tornado.httpserver.HTTPServer(application,
+                                              ssl_options={
+                                                "certfile": config.get('domain_cert_and_key','DOMAIN_CERT'),
+                                                "keyfile": config.get('domain_cert_and_key','DOMAIN_KEY')
+                                                }
+                                              )
   http_server.listen(8080)
   tornado.ioloop.IOLoop.instance().start()
+
+
+
